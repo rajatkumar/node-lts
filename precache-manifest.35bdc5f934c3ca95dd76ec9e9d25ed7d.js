9bf5b7ee2dcef58f669f4ac875523847d99689f2
Rajat Kumar <rajatk@netflix.com>
2019-02-28 13:07:35 -0800

    Updates

---
+++ precache-manifest.35bdc5f934c3ca95dd76ec9e9d25ed7d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b658037bb036f525a7de",
+    "url": "/static/css/main.74383392.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "3950aea37bb40e29b7ef",
+    "url": "/static/js/2.3950aea3.chunk.js"
+  },
+  {
+    "revision": "b658037bb036f525a7de",
+    "url": "/static/js/main.b658037b.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "3950aea37bb40e29b7ef",
+    "url": "/static/css/2.d30276a6.chunk.css"
+  },
+  {
+    "revision": "5a38b5a5d1561b27ac0596add82f371c",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/node-lts/precache-manifest.ea0d983743fa9e8496cc739ad2f0bbbd.js"
+  "/precache-manifest.35bdc5f934c3ca95dd76ec9e9d25ed7d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/node-lts/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
